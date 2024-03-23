6a8732eb05e2e48c4033909829fa57ab765a32f7
Testerhubplayer <157597648+Testerhubplayer@users.noreply.github.com>
2024-03-23 15:41:08 +0800

    Update RipBaller12306Hub.lua

--- RipBaller12306Hub.lua
+++ RipBaller12306Hub.lua
@@ -1,33 +1,183 @@
-local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local win = Flux:Window("awesome script", "Slap battles", Color3.fromRGB(0, 255, 0), Enum.KeyCode.LeftControl)
-local tab = win:Tab("Main", "http://www.roblox.com/asset/?id=6023426915")
-tab:Button("Revive character", "Not works", function()
-Flux:Notification("It not works now!", "Ok")
-end)
-tab:Button("Say slap battles is cool", "Says slap battles is cool", function()
-            if game.ReplicatedStorage.DefaultChatSystemChatEvents:FindFirstChild("SayMessageRequest") then
-              game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Slap battles is cool", "All")
-            end
-end)
-tab:Line()
-tab:Slider("Walkspeed", "Makes your faster.", 20, 50, 20,function(t)
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = t
-end)
-tab:Colorpicker("Body color", Color3.fromRGB(255,255,255), function(t)
-game.Players.LocalPlayer.Character.Head.Color = t
-game.Players.LocalPlayer.Character.Torso.Color = t
-game.Players.LocalPlayer.Character["Left Arm"].Color = t
-game.Players.LocalPlayer.Character["Left Leg"].Color = t
-game.Players.LocalPlayer.Character["Right Arm"].Color = t
-game.Players.LocalPlayer.Character["Right Leg"].Color = t
-end)
-tab:Textbox("Notes", "You can see your notes by pressing F9.", true, function(t)
-print(t)
-end)
-tab:Button("Rhythm explosion", "Spawns Rhythm explosion", function()
-game:GetService("ReplicatedStorage").rhythmevent:FireServer("AoeExplosion",0)
-end)
-tab:Button("Esp", "You cant turn it off", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PopCat0881/PopCat0881/main/Esp.lua"))()
-end)
+local Window = OrionLib:MakeWindow({Name = "Poor gloves hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionGlovesHub"})
+OrionLib:MakeNotification({
+	Name = "Notification",
+	Content = "Script loaded!",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+local Tab = Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "Misc"
+})
+Tab:AddButton({
+	Name = "Clear kick error",
+	Callback = function()
+      		game:GetService("GuiService"):ClearError()
+  	end    
+})
+Tab:AddButton({
+	Name = "Make the glove black (Works only with default)",
+	Callback = function()
+      		game.Players.LocalPlayer.Backpack.Default.Glove.Color = Color3.fromRGB(000, 000, 000)
+  	end    
+})
+Tab:AddButton({
+	Name = "Turn glove into a block (Works only with default)",
+	Callback = function()
+      		game.Players.LocalPlayer.Backpack.Default.Glove.Mesh:Destroy()
+  	end    
+})
+Tab:AddToggle({
+	Name = "Auto slap (For default)",
+	Default = false,
+	Callback = function(Value)
+		_G.SlapAura = Value
+		while _G.SlapAura do
+		   game.Players.LocalPlayer.Character.Default:Activate()
+		   wait(0.3)
+		end
+	end    
+})
+Tab:AddToggle({
+	Name = "Auto slap (For God's hand)",
+	Default = false,
+	Callback = function(Value)
+		_G.SlapAura = Value
+		while _G.SlapAura do
+		   game.Players.LocalPlayer.Character["God's hand"]:Activate()
+		   wait(0.1)
+		end
+	end    
+})
+Tab:AddToggle({
+	Name = "Auto slap (For OVERKILL)",
+	Default = false,
+	Callback = function(Value)
+		_G.SlapAura = Value
+		while _G.SlapAura do
+		   game.Players.LocalPlayer.Character.OVERKILL:Activate()
+		   wait(0.1)
+		end
+	end    
+})
+Tab:AddButton({
+	Name = "Turn on anti void",
+	Callback = function()
+      		game.Workspace.dedBarrier.CanCollide = true
+      		game.Workspace.dedBarrier.Transparency = 1
+      		game.Workspace.dedBarrier.Material = "Slate"
+      		game.Workspace.dedBarrier.Color = Color3.fromRGB(105, 64, 40)
+            game.Workspace.TAntiVoid.CanCollide = true
+            game.Workspace.TAntiVoid.Transparency = 1
+  	end    
+})
+Tab:AddButton({
+	Name = "Turn off anti void",
+	Callback = function()
+      		game.Workspace.dedBarrier.CanCollide = false
+      		game.Workspace.dedBarrier.Transparency = 1
+            game.Workspace.TAntiVoid.CanCollide = false
+            game.Workspace.TAntiVoid.Transparency = 1
+  	end    
+})
+Tab:AddButton({
+	Name = "Teleport to plate (buggy)",
+	Callback = function()
+      		game.Players.LocalPlayer.Character.HumanoidRootPart.Position = game.Workspace.Arena.Plate.Sign.Text.Position
+  	end    
+})
+Tab:AddToggle({
+	Name = "Rhythm explosion spam (Works with any glove)",
+	Default = false,
+	Callback = function(Value)
+		_G.RhythmSpam = Value
+		while _G.RhythmSpam do
+		   game:GetService("ReplicatedStorage").rhythmevent:FireServer("AoeExplosion",3)
+           task.wait()
+		end
+	end    
+})
+Tab:AddToggle({
+	Name = "Brick spam",
+	Default = false,
+	Callback = function(Value)
+		_G.BrickSpam = Value
+		while _G.BrickSpam do
+		   game:GetService("ReplicatedStorage").lbrick:FireServer()
+		   task.wait(1.1)
+		end
+	end    
+})
+Tab:AddToggle({
+	Name = "Slow Brick spam (Use if high ping)",
+	Default = false,
+	Callback = function(Value)
+		_G.SlowBrickSpam = Value
+		while _G.SlowBrickSpam do
+		   game:GetService("ReplicatedStorage").lbrick:FireServer()
+		   task.wait(5.1)
+		end
+	end    
+})
+Tab:AddToggle({
+	Name = "Rojo spam (Works with any glove)",
+	Default = false,
+	Callback = function(Value)
+		if Person == nil then
+Person = game.Players.LocalPlayer.Name
+end
+_G.RojoSpam = Value
+while _G.RojoSpam do
+game:GetService("ReplicatedStorage"):WaitForChild("RojoAbility"):FireServer("Release", {game.Players[Person].Character.HumanoidRootPart.CFrame})
+task.wait(2)
+end
+	end    
+})
+Tab:AddButton({
+	Name = "Equip default",
+	Callback = function()
+      		fireclickdetector(game.Workspace.Lobby.Default.ClickDetector)
+  	end    
+})
+Tab:AddButton({
+	Name = "Equip brick",
+	Callback = function()
+      		fireclickdetector(game.Workspace.Lobby.Brick.ClickDetector)
+  	end    
+})
+Tab:AddButton({
+	Name = "Overkill prompt",
+	Callback = function()
+      		fireclickdetector(game.Workspace.Lobby.OVERKILL.ClickDetector)
+  	end    
+})
+Tab:AddButton({
+	Name = "Equip error",
+	Callback = function()
+      		fireclickdetector(game.Workspace.Lobby.Error.ClickDetector)
+  	end    
+})
+Tab:AddToggle({
+	Name = "Anti brazil portal (On is off, Off is on)",
+	Default = true,
+	Callback = function(Value)
+		game.Workspace.Lobby.brazil.portal.CanTouch = Value
+	end    
+})
+Tab:AddButton({
+	Name = "Kick Player (Equip za hando)",
+	Callback = function()
+      		game:GetService("ReplicatedStorage").Erase:FireServer()
+                wait(0.47)
+                game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 0
+                game.Players.LocalPlayer.Character.Humanoid.JumpPower = 0
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Spot.CFrame * CFrame.new(1022,213.8,1498)
+                game.Workspace.Lobby.brazil.portal.CanTouch = false
+  	end    
+})
